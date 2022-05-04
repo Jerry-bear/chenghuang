6ea6a7a612ea4468a9d601404beebecc1e1361e4
Jerry-bear <1030275179@qq.com>
2022-05-04 10:09:02 +0800

    手机发送mqtt控制机器

--- app/src/main/java/com/permissionx/xuewei/MainActivity.kt
+++ app/src/main/java/com/permissionx/xuewei/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -16,7 +17,16 @@ class MainActivity : AppCompatActivity() {
     val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_PHONE_STATE)
     val mPermissionList = ArrayList<String>()
     val mRequestCode = 0x1
-    private lateinit var mqttSend:Button
+    private lateinit var yde_btn:Button
+    private lateinit var yadd_btn:Button
+    private lateinit var xde_btn:Button
+    private lateinit var xadd_btn:Button
+    private lateinit var dyde_btn:Button
+    private lateinit var dyadd_btn:Button
+    private lateinit var dxde_btn:Button
+    private lateinit var dxadd_btn:Button
+    private lateinit var xychange_edt:EditText
+    private lateinit var dxychange_edt:EditText
     private lateinit var mqtt:MQTTClient
 
 
@@ -24,7 +34,16 @@ class MainActivity : AppCompatActivity() {
         initPermission()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mqttSend = findViewById(R.id.mqtt_send)
+        yde_btn = findViewById(R.id.mainact_btn_yde)
+        yadd_btn = findViewById(R.id.mainact_btn_yadd)
+        xde_btn = findViewById(R.id.mainact_btn_xde)
+        xadd_btn = findViewById(R.id.mainact_btn_xadd)
+        dyde_btn = findViewById(R.id.mainact_btn_dyde)
+        dyadd_btn = findViewById(R.id.mainact_btn_dyadd)
+        dxde_btn = findViewById(R.id.mainact_btn_dxde)
+        dxadd_btn = findViewById(R.id.mainact_btn_dxadd)
+        xychange_edt = findViewById(R.id.mainact_edt_xychange)
+        dxychange_edt = findViewById(R.id.mainact_edt_dxychange)
         initListener()
         //mqtt使用例子
         mqtt= MQTTClient.newInstance(this)
@@ -55,12 +74,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initListener(){
-        mqttSend.setOnClickListener {
-            mqtt.publish("jerry sb")
+
+        yde_btn.setOnClickListener {
+            val text=xychange_edt.text
+            mqtt.publish("y-"+text)
+        }
+        yadd_btn.setOnClickListener {
+            val text=xychange_edt.text
+            mqtt.publish("y+"+text)
+        }
+        xde_btn.setOnClickListener {
+            val text=xychange_edt.text
+            mqtt.publish("x-"+text)
+        }
+        xadd_btn.setOnClickListener {
+            val text=xychange_edt.text
+            mqtt.publish("x+"+text)
+        }
+        dyde_btn.setOnClickListener {
+            val text=dxychange_edt.text
+            mqtt.publish("dy-"+text)
+        }
+        dyadd_btn.setOnClickListener {
+            val text=dxychange_edt.text
+            mqtt.publish("dy+"+text)
+        }
+        dxde_btn.setOnClickListener {
+            val text=dxychange_edt.text
+            mqtt.publish("dx-"+text)
+        }
+        dxadd_btn.setOnClickListener {
+            val text=dxychange_edt.text
+            mqtt.publish("dx+"+text)
         }
     }
 
     private fun initView(){
-        mqttSend = findViewById(R.id.mqtt_send)
+        yde_btn = findViewById(R.id.mainact_btn_yde)
     }
 }
---
+++ app/src/main/java/com/permissionx/xuewei/RecognitionActivity.kt
@@ -0,0 +1,15 @@
+package com.permissionx.xuewei
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecognitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recognition)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
